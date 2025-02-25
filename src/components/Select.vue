1b8f7da0e632baf22a8d571032e56cb5c543ccc5
upfloor5 <lipli@jetfuel.com.tw>
2025-02-25 16:59:47 +0800

    feat: update select structure

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,51 +1,22 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { ref } from 'vue';
-import axios from 'axios';
+import { storeToRefs } from 'pinia';
 import { useSelectStore } from '../store/selectStore';
 
-const options = ref({
-  type: [
-    { label: '澳門六合彩', value: '1' },
-    { label: '澳門六十彩', value: '3' },
-    { label: '香港六合彩', value: '2' },
-    { label: '香港六合彩', value: '4' }
-  ],
-  year: [
-    { label: '2025', value: '2025' },
-    { label: '2024', value: '2024' }
-  ],
-  period: [
-    { label: '最新50期', value: '50' },
-    { label: '最新100期', value: '100' },
-    { label: '最新150期', value: '150' },
-    { label: '最新200期', value: '200' },
-    { label: '全年', value: '366' }
-  ]
-});
-
-const typeDefaultValue = ref<string>('澳門六合彩');
-const yearDefaultValue = ref<string>('2025');
-const periodDefaultValue = ref<string>('最新50期');
-
-const selectStore = useSelectStore();
-
-// 定義 props
-const props = defineProps({
-  modelValue: String,
-  items: Array
-});
+const store = useSelectStore();
+// 使用 storeToRefs 來保持響應性
+const { type, year, period, selectOptions } = storeToRefs(store);
+const { getTypeAPIData, getYearAPIData, getPeriodAPIData } = store;
 </script>
 
 <template>
-  <el-select v-model="typeDefaultValue" class="select-width" :fallback-placements="['bottom-start']">
-    <el-option v-for="item in options.type" :key="item" :label="item.label" :value="item.value" @click="selectStore.getTypeAPIData(item.value)" />
+  <el-select v-model="type" class="select-width" :fallback-placements="['bottom-start']">
+    <el-option v-for="item in selectOptions.type" :key="item" :label="item.label" :value="item.value" @click="getTypeAPIData(item.value)" />
   </el-select>
-  <el-select v-model="yearDefaultValue" class="select-width" :fallback-placements="['bottom-start']">
-    <el-option v-for="item in options.year" :key="item" :label="item.label" :value="item.value" @click="selectStore.getYearAPIData(item.value)" />
+  <el-select v-model="year" class="select-width" :fallback-placements="['bottom-start']">
+    <el-option v-for="item in selectOptions.year" :key="item" :label="item.label" :value="item.value" @click="getYearAPIData(item.value)" />
   </el-select>
-  <el-select v-model="periodDefaultValue" class="select-width" :fallback-placements="['bottom-start']">
-    <el-option v-for="item in options.period" :key="item" :label="item.label" :value="item.value" @click="selectStore.getPeriodAPIData(item.value)" />
+  <el-select v-model="period" class="select-width" :fallback-placements="['bottom-start']">
+    <el-option v-for="item in selectOptions.period" :key="item" :label="item.label" :value="item.value" @click="getPeriodAPIData(item.value)" />
   </el-select>
 </template>
 
